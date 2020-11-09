ecd60fc432608533684edce25dc0cf8bd9ccdf4c
michael <mpthornton86@gmail.com>
2020-11-09 11:15:56 -0700

    commit tree 48c2a2b71303c08d25f78c2bc0a27a7d5e0ab014

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,68 @@
+require 'pry'
+require_relative 'cat'
 class Owner
-  # code goes here
+  attr_reader :name, :species
+  @@all = []
+
+  def initialize(name, species= "human")
+    @name = name
+    @species = species
+    @@all << self
+  end
+  def say_species
+    "I am a #{species}."
+  end
+  def self.all
+    @@all
+  end
+  def self.count
+    @@all.count
+  end
+  def self.reset_all
+    @@all.clear
+  end
+  def cats
+    Cat.all.select {|cat| cat.owner == self}
+  end
+  def dogs
+    Dog.all.select {|dog| dog.owner == self}
+  end
+  def buy_cat(name)
+    Cat.new(name, self)    
+  end
+  def buy_dog(name)
+    Dog.new(name, self)
+  end
+  def walk_dogs
+    Dog.all.each do |dog| 
+      if dog.owner == self
+        dog.mood = "happy"
+      end
+    end
+  end
+  def feed_cats
+    Cat.all.each do |cat|
+      if cat.owner == self
+        cat.mood = "happy"
+      end
+    end
+  end
+  def sell_pets
+    Cat.all.each do |cat|
+      if cat.owner == self
+        cat.owner = nil
+        cat.mood = "nervous"
+      end
+    end
+    Dog.all.each do |dog| 
+      if dog.owner == self
+        dog.owner = nil
+        dog.mood = "nervous" 
+      end
+    end
+  end
+  def list_pets
+    "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
+  end
 end
+#binding.pry
